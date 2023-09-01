c2bb0a29c03bdd72d0e43fe06d155e02975247ac
Guilherme Zanin <guizanin224@gmail.com>
2023-09-01 16:32:59 +0100

    added favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel='icon' href='/favicon.ico' />
+        <link rel='icon' href='/favicon-taskbar.ico' />
+        <link rel='icon' href='/favicon-desktop.ico' />
+        <link rel='icon' href='/favicon-apple.ico' />
+        <link rel='icon' href='/favicon-wordpress.ico' />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
