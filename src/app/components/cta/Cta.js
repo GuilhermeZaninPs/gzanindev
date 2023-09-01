deec0afcbb40598769bd43b1a91d86ce3251370c
Guilherme Zanin <guizanin224@gmail.com>
2023-09-01 18:35:02 +0100

    updated resume to download

--- src/app/components/cta/Cta.js
+++ src/app/components/cta/Cta.js
@@ -8,7 +8,7 @@ export default function Cta () {
                     <h1 className="text-[calc(1.56vw+1.56rem)] lg:text-2.81rem font-bold">Download CV</h1>
                     <h3 className="text-[calc(0.44vw+0.95rem)] lg:text-1.19rem">Download my CV to learn more about me</h3>
                 </div>
-                <a href="/Cv.pdf" download="Resume_Guilherme_Zanin" target="_blank"><button className="bg-[#2896df] px-4 py-2 h-12 w-36 font-bold">Download</button></a>
+                <a href="/resume_guilherme_zanin.pdf" download="Resume_Guilherme_Zanin" target="_blank"><button className="bg-[#2896df] px-4 py-2 h-12 w-36 font-bold">Download</button></a>
             </section>
         </>
     )
