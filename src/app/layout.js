99af44ce7900ef8539c62cd3784c538b4e9adee7
Guilherme Zanin <guizanin224@gmail.com>
2023-12-27 16:10:19 +0000

    added favicon to public folder and updated description

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,18 +5,18 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'GZanin Dev',
-  description: 'Frontend portfolio',
+  description: 'Guilherme Zanin - Frontend portfolio',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel='icon' href='/favicon.ico' />
-        <link rel='icon' href='/favicon-taskbar.ico' />
-        <link rel='icon' href='/favicon-desktop.ico' />
-        <link rel='icon' href='/favicon-apple.ico' />
-        <link rel='icon' href='/favicon-wordpress.ico' />
+        <link rel='icon' href='./favicon.ico' />
+        <link rel='icon' href='./favicon-taskbar.ico' />
+        <link rel='icon' href='./favicon-desktop.ico' />
+        <link rel='icon' href='./favicon-apple.ico' />
+        <link rel='icon' href='./favicon-wordpress.ico' />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
